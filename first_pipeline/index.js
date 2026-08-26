b19e5f239ebc394d1d96d832b7dac4838dc3120e
giaphu39 <trangiaphu39@gmail.com>
2026-08-26 23:42:50 +0700

    feat: add CI first pipeline

---
+++ first_pipeline/index.js
@@ -0,0 +1,2 @@
+console.log("Hello devops");
+console.log("I am a develope");
---
+++ first_pipeline/test.js
@@ -0,0 +1,3 @@
+console.log('starting tests...')
+setTimeout(() => console.log('waiting 2 seconds...'), 2000)
+console.log('tests finished!')
