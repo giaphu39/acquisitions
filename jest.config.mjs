5c6c44d6558b1f63723b597683f1a71bfe688afe
giaphu39 <trangiaphu39@gmail.com>
2026-08-27 00:05:14 +0700

    fix pipeline

--- jest.config.mjs
+++ jest.config.mjs
@@ -164,7 +164,8 @@ const config = {
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
     "/node_modules/",
-    "<rootDir>/first_pipeline/"
+    "<rootDir>/first_pipeline/",
+    "<rootDir>/learning/"
   ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
